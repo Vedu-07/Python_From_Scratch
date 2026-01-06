9e3e188346e98f198d3b7fd93c96fa2e3f48c816
Vedu-07 <vedantswami02@gmail.com>
2026-01-06 23:07:18 +0530

    Tuple datatype

---
+++ Data_Types/tuple_demo.py
@@ -0,0 +1,12 @@
+tea_types = ("Black", "Green", "Oolong")
+print(tea_types)
+
+more_tea = ("Herbal", "Earl Grey")
+print(more_tea)
+
+all_tea = more_tea + tea_types
+print(all_tea)
+
+(black, green, oolong) = tea_types
+
+print(black)
